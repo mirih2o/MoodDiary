53c13850ddfab1fb393a6f7c30f4f7c3a3e3867f
mirih2o <mirih2o@users.noreply.github.com>
2024-05-16 14:03:56 +0200

    Add PieChart
    Add a PieChart from Recharts

--- src/BottomTab/Analytic.js
+++ src/BottomTab/Analytic.js
@@ -1,11 +1,30 @@
+import React from 'react';
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { PieChart, Pie, Cell, Tooltip } from 'recharts';
+
+const data = [
+  { name: '5 Very happy', amount: 1 },
+  { name: '4 Happy', amount: 2 },
+  { name: '3 Neutral', amount: 3 },
+  { name: '2 Sad', amount: 1 },
+  { name: '1 Very sad', amount: 1 }
+];
+const COLORS = ['#00D530', '#C5CD10', '#FFDD00', '#EEAA08', '#F02222'];
 
 export default function Analytic() {
   return (
     <View style={styles.container}>
       <Text>Analytic!</Text>
+      <PieChart width={400} height={400} onMouseEnter={this.onPieEnter}>
+        <Pie dataKey="amount" data={data} cx={200} cy={200} outerRadius={80}>
+        {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Tooltip />
+      </PieChart>
       <StatusBar style="auto" />
     </View>
   );
